06deedf4395e1e8a1d113d3b9a45580a397fba0b
kimmincastle <koreakms0810@gmail.com>
2018-03-09 10:18:29 +0900

    프로젝트 의뢰하기 첫 페이지

--- application/modules/translation_order/views/base/selectTranstionType.php
+++ application/modules/translation_order/views/base/selectTranstionType.php
@@ -1,2 +1,62 @@
-<a href="/translation_order/add?type=통역">통역</a>
-<a href="/translation_order/add?type=번역">번역</a>
+<section class="home-hero">
+    <h2 class="home-hero-title">대한민국 대표<br>언어 전문 서비스 기업</h2>
+    <p class="home-hero-des">
+        코리아 통번역 센터는 전문 번역 서비스는 물론<br>
+        소프트웨어 현지화, 국제회의 맞춤 통역 서비스 등을 제공하는<br>
+        대한민국 대표 언어 전문 서비스 기업입니다
+    </p>
+    <a href="/translation_order/selectType" class="home-btn">프로젝트 의뢰하기</a>
+</section>
+
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/jquery.bxslider.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/swiper.min.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-base.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-style.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-media.css" media="all">
+<script src="/public/subpage/css/001_jobdance/js/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script src="/public/subpage/css/001_jobdance/js/jquery.bxslider.min.js" type="text/javascript"></script>
+<script src="/public/subpage/css/001_jobdance/js/swiper.min.js" type="text/javascript"></script>
+<script src="/public/subpage/css/001_jobdance/js/front-ui.js" type="text/javascript"></script>
+
+<div class="wrap">
+    <div class="sub-content">
+        <div class="join-wrap">
+            <div class="join-tit">
+                코리아 통번역 센터는 <b>318건</b>의 <span>프로젝트</span>를 하였고,<br>
+                <b>104개</b>의 <span>통번역사</span>와 함께합니다
+            </div>
+            <div class="join-list">
+                <ul>
+                    <li>
+                        <div>
+                            <a href="/translation_order/add?type=통역">
+                                <div class="poi">
+                                    <div class="img"><span><img src="/public/subpage/css/001_jobdance/images/j-icon01.png" alt=""></span></div>
+                                    <div class="name">통역</div>
+                                    <div class="txt">
+                                        뛰어난 인재를 찾으려 하는 <br class="one">사업자나 사업채 직원이 속합니다. <br>채용 정보를 등록하고 <br class="one">인재 정보를 조회할 수 있습니다.
+                                    </div>
+                                </div>
+                                <div class="btn">의뢰하기</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <a href="/translation_order/add?type=번역">
+                                <div class="poi">
+                                    <div class="img"><span><img src="/public/subpage/css/001_jobdance/images/j-icon02.png" alt=""></span></div>
+                                    <div class="name">번역</div>
+                                    <div class="txt">
+                                        일자리를 구하려 하는 <br class="one">취업 준비생 본인이 속합니다. <br>이력서를 작성해 인재 정보를 등록하고 <br class="one">채용 정보를 조회할 수 있습니다.
+                                    </div>
+                                </div>
+                                <div class="btn">의뢰하기</div>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>	
+    </div>
+</div>
