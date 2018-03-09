710decbc7f6af9b06773a4acd0d134319792b58b
임수빈 <sbin910@gmail.com>
2018-03-09 18:00:14 +0900

    기밀유지정책

--- application/modules/small/views/base/security.php
+++ application/modules/small/views/base/security.php
@@ -1,1 +1,52 @@
-기밀 유지 정책
+<section class="home-hero-project">
+    <h2 class="home-hero-title-project">기밀 유지 정책</h2>
+    <p class="home-hero-des-project">
+        고객님의 의뢰에 최선을 다하는 코리아 통번역 센터입니다!
+    </p>
+    <a href="/small/portfolio_list" class="home-btn">포트폴리오 보러가기</a>
+</section>
+
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/jquery.bxslider.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/swiper.min.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-base.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-style.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-media.css" media="all">
+<script src="/public/subpage/css/001_jobdance/js/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script src="/public/subpage/css/001_jobdance/js/jquery.bxslider.min.js" type="text/javascript"></script>
+<script src="/public/subpage/css/001_jobdance/js/swiper.min.js" type="text/javascript"></script>
+<script src="/public/subpage/css/001_jobdance/js/front-ui.js" type="text/javascript"></script>
+
+<div class="wrap">
+    <div class="sub-content">
+        
+        <div class="join-wrap">
+            <div class="join-tit">
+                <b>코리아 통번역 센터 기밀 유지 보안 정책 안내</b><br><br>
+                코리아 통번역센터는 사내외 보안정책을 실시하고 있으며,<br>
+                고객님의 기밀사항을 철저하게 보호해 드리고 있습니다.<br>
+                NDA(Non-Disclosure Agreement : 기밀유지협약)가 체결된 중요 문서군<br>
+                프로젝트 단위로 각기 다른 문서보안 System을 통하여 관리, 보호하고 있으며,<br>
+                프로젝트 종료 후 일정기간 내에 파기를 원칙으로 하고 있습니다.
+            </div>
+            <div class="join-list">
+                <ul>
+                    <li>
+                        <div>
+                            <a href="/public/images/portfolio-1.png" download>
+                                <div class="poi">
+                                    <div class="img"><span><img src="/public/subpage/css/001_jobdance/images/j-icon01.png" alt=""></span></div>
+                                    <div class="name">기밀 유지</div>
+                                    <div class="txt">
+                                        군영과 곧 인생에 끝까지 듣는다.<br class="one">
+                                        용감하고 품고 위하여서, 사라지지 것이다.
+                                    </div>
+                                </div>
+                                <div class="btn">다운로드</div>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>	
+    </div>
+</div>
