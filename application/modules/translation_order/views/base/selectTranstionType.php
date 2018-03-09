c176b7b2f8078ec5edc71b39961fa19528701319
kimmincastle <koreakms0810@gmail.com>
2018-03-09 11:53:49 +0900

    end

--- application/modules/translation_order/views/base/selectTranstionType.php
+++ application/modules/translation_order/views/base/selectTranstionType.php
@@ -1,15 +1,16 @@
 <section class="home-hero">
-    <h2 class="home-hero-title">대한민국 대표<br>언어 전문 서비스 기업</h2>
+    <h2 class="home-hero-title">프로젝트 의뢰하기</h2>
     <p class="home-hero-des">
-        코리아 통번역 센터는 전문 번역 서비스는 물론<br>
-        소프트웨어 현지화, 국제회의 맞춤 통역 서비스 등을 제공하는<br>
-        대한민국 대표 언어 전문 서비스 기업입니다
+        코리아 통번역 센터는<br>
+        318건의 프로젝트를 맡았고,<br>
+        103명의 통번역사와 함께하고 있습니다
     </p>
-    <a href="/translation_order/selectType" class="home-btn">프로젝트 의뢰하기</a>
+    <a href="/small/portfolio_list" class="home-btn">포트폴리오 보러가기</a>
 </section>
 
 <link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/jquery.bxslider.css" media="all">
 <link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/swiper.min.css" media="all">
+<link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-base.css" media="all">
 <link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-style.css" media="all">
 <link rel="stylesheet" type="text/css" href="/public/subpage/css/001_jobdance/css/front-media.css" media="all">
 <script src="/public/subpage/css/001_jobdance/js/jquery-1.11.3.min.js" type="text/javascript"></script>
@@ -19,10 +20,34 @@
 
 <div class="wrap">
     <div class="sub-content">
+        <div class="join-navi">
+            <div>
+                <ul>
+                    <li class="on">
+                        <div class="fix">
+                            <div class="bom">1</div>
+                            <p>분야 선택</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="fix">
+                            <div class="bom">2</div>
+                            <p>정보 작성</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="fix">
+                            <div class="bom">3</div>
+                            <p>의뢰 완료</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
         <div class="join-wrap">
             <div class="join-tit">
-                코리아 통번역 센터는 <b>318건</b>의 프로젝트를 진행하였고,<br>
-                <b>104명</b>의 통번역사와 함께합니다
+                지금 가입하면 <b>318개</b>의 단체 회원, <b>2,487명</b>의 개인 회원<br>
+                그리고 <b>104개</b>의 기업 회원이 함께합니다.
             </div>
             <div class="join-list">
                 <ul>
@@ -48,8 +73,8 @@
                                     <div class="img"><span><img src="/public/subpage/css/001_jobdance/images/j-icon02.png" alt=""></span></div>
                                     <div class="name">번역</div>
                                     <div class="txt">
-                                        군영과 곧 인생에 끝까지 듣는다.<br class="one">
-                                        용감하고 품고 위하여서, 사라지지 것이다.
+                                        용감하고 품고 위하여서, 사라지지 것이다.<br class="one">
+                                        군영과 곧 인생에 끝까지 듣는다.
                                     </div>
                                 </div>
                                 <div class="btn">의뢰하기</div>
