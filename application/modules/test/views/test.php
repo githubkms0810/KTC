946b499d5be88691bbb60c3826cc930545772e3c
santutu <santutu@naver.com>
2018-03-12 12:47:12 +0900

    ㅇ

--- application/modules/test/views/test.php
+++ application/modules/test/views/test.php
@@ -1,6 +1,6 @@
 
 <form action="" method="post">
-<!-- label로 전체를 감싸면 css 내부적으로 뭔가 꼬여셔 for=""를 추가해야되는것 같음 -->
+  <!-- <label >성별 -->
   <label for="">성별
     <input type="radio" name="sex" value="여성" id="a" >
     <label for="a">여성</label>
@@ -9,3 +9,17 @@
   </label>
   <input type="submit">
 </form>
+
+<!-- input 태그의 id와 label의 for를 맞춰주면 둘이 연결됨 -->
+<label for="c">클릭시 하면체크</label>
+<div>내용없음</div>
+<input type="radio" name="sex" value="여성" id="c" >
+
+<br>
+<br>
+
+<!-- label태그안에 input태그를 넣으면 자동으로 연결됨-->
+<label>
+  <input type="radio" name="sex" value="남성" >
+  클릭하면체크
+</label>
