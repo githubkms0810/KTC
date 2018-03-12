f99fa2f17631b2c90e82291a44b46d6aa0870d83
santutu <santutu@naver.com>
2018-03-12 12:40:10 +0900

    ㅇ

--- application/modules/test/views/test.php
+++ application/modules/test/views/test.php
@@ -1,10 +1,11 @@
 
 <form action="" method="post">
-<label for="">성별
-  <input type="radio" name="sex" value="여성" id="a" >
-<label for="a">여성</label>
-  <input type="radio" name="sex" value="남성" id="b">
-<label for="b">남성</label>
+<!-- label로 전체를 감싸면 css 내부적으로 뭔가 꼬여셔 for=""를 추가해야되는것 같음 -->
+  <label for="">성별
+    <input type="radio" name="sex" value="여성" id="a" >
+    <label for="a">여성</label>
+    <input type="radio" name="sex" value="남성" id="b">
+    <label for="b">남성</label>
   </label>
-<input type="submit">
+  <input type="submit">
 </form>
