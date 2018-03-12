2718dd6b9c44abce6b6baddd874253cbfece59d9
santutu <santutu@naver.com>
2018-03-12 18:35:29 +0900

    ㅁㅇㅁㅇ

--- application/modules/test/views/test.php
+++ application/modules/test/views/test.php
@@ -1,25 +1,16 @@
+<div class="scroll" id="autoScroll"> <div> <img src="./images/IMGP8563.JPG" width="400" /> <img src="./images/IMGP8564.JPG" width="400" /> </div> <div class="next"><a href="contents.php?mode=auto&page=2" class="nextPage">다음 페이지</a></div> </div>
 
-<form action="" method="post">
-  <!-- <label >성별 -->
-  <label for="">성별
-    <input type="radio" name="sex" value="여성" id="a" >
-    <label for="a">여성</label>
-    <input type="radio" name="sex" value="남성" id="b">
-    <label for="b">남성</label>
-  </label>
-  <input type="submit">
-</form>
 
-<!-- input 태그의 id와 label의 for를 맞춰주면 둘이 연결됨 -->
-<label for="c">클릭시 하면체크</label>
-<div>내용없음</div>
-<input type="radio" name="sex" value="여성" id="c" >
 
-<br>
-<br>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="./jquery.jscroll.js"></script>
+<script type="text/javascript">
 
-<!-- label태그안에 input태그를 넣으면 자동으로 연결됨-->
-<label>
-  <input type="radio" name="sex" value="남성" >
-  클릭하면체크
-</label>
+ $(document).ready(function () {
+    $('#autoScroll').jscroll({ 
+      autoTrigger: true, 
+      loadingHtml: '<div class="next"><img src="./images/loading.gif" alt="Loading" /></div>', 
+      nextSelector: 'a.nextPage:last' });
+}); 
+</script>
+
