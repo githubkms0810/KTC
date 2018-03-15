bb9a2ef05519cf2671cc029305aa0250f9b185d6
kimmincastle <koreakms0810@gmail.com>
2018-03-15 17:18:04 +0900

    업무 제휴

--- application/modules/contact/views/base/addUpdate.php
+++ application/modules/contact/views/base/addUpdate.php
@@ -36,38 +36,38 @@
 			</button>
 
 			<form action="/contact/add" method="post" class="contact100-form validate-form">
-				<span class="contact100-form-title">
+				<span class="contact100-form-title" style="font-weight:bold;">
 					제휴 문의
 				</span>
 
 				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="회사명을 입력해주세요.">
 					<span class="label-input100">회사</span>
-					<input class="input100" type="text" name="company_name" placeholder="회사명" value="<?=DEBUG === true ? "테스트회사명": ""?>">
+					<input class="input100" type="text" name="company_name" value="<?=DEBUG === true ? "테스트회사명": ""?>">
 					<span class="focus-input100"></span>
 				</div>
 
-				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate = "이메일을 입력해주세요. 예)example@email.com">
-					<span class="label-input100">이메일</span>
-					<input class="input100" type="text" name="email" placeholder="이메일" value="<?=DEBUG === true ? "test@email.com": ""?>">
+				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate = "E-mail을 입력해주세요.">
+					<span class="label-input100">E-mail</span>
+					<input class="input100" type="text" name="email" value="<?=DEBUG === true ? "test@email.com": ""?>">
 					<span class="focus-input100"></span>
 				</div>
 
 				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="담당자명을 입력해주세요.">
-					<span class="label-input100">담당자 이름</span>
-					<input class="input100" type="text" name="manager" placeholder="담당자명" value="<?=DEBUG === true ? "테스트 담당자명": ""?>">
+					<span class="label-input100">담당자명</span>
+					<input class="input100" type="text" name="manager" value="<?=DEBUG === true ? "테스트 담당자명": ""?>">
 					<span class="focus-input100"></span>
 				</div>
 
 				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="담당자 연락처를 입력해주세요.">
 					<span class="label-input100">담당자 연락처</span>
-					<input class="input100" type="text" name="manager_phone" placeholder="담당자 연락처" value="<?=DEBUG === true ? "테스트 담당자 연락처": ""?>">
+					<input class="input100" type="text" name="manager_phone" value="<?=DEBUG === true ? "테스트 담당자 연락처": ""?>">
 					<span class="focus-input100"></span>
 				</div>
 
 
 				<div class="wrap-input100 validate-input" data-validate = "내용을 입력해주세요.">
 					<span class="label-input100">내용</span>
-					<textarea class="input100" name="desc" placeholder="내용을 입력해주세요."><?=DEBUG === true ? "테스트메세지": ""?></textarea>
+					<textarea class="input100" name="desc"><?=DEBUG === true ? "테스트메세지": ""?></textarea>
 					<span class="focus-input100"></span>
 				</div>
 
@@ -82,7 +82,7 @@
 			</form>
 
 			<span class="contact100-more">
-				빠른 답변을 원하시면 콜 센터로 문의해주세요. <span class="contact100-more-highlight">02-6738-8220</span>
+				빠른 답변을 원하시면 전화로 문의해주세요! <span class="contact100-more-highlight">02-6738-8220</span>
 			</span>
 		</div>
 	</div>
