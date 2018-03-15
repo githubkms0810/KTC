72fd033991baa65ff571800c9b8e6d40bdfceda6
kimmincastle <koreakms0810@gmail.com>
2018-03-15 12:08:39 +0900

    고객센터 로고 유격

--- application/views/base/template.php
+++ application/views/base/template.php
@@ -17,7 +17,7 @@
         <header class="home-header">
             <div class="bc-container">
                 <h1 class="home-header__logo">
-                    <a href="/"><img src="/public/images/logo.png" alt="KTC"></a>
+                    <a href="/" style="font-size: 28px !important;"><img src="/public/images/logo.png" style="vertical-align: baseline;" alt="KTC"></a>
                 </h1>
                 <span class="home-nav--btn">
                     <img src="/public/images/menu.png" alt="nav">
