fd757ea544df35d966e0f580ca583faf32e83fec
santutu <santutu@naver.com>
2018-03-16 19:12:14 +0900

    ㅇ

--- application/modules/freelancer/controllers/Base.php
+++ application/modules/freelancer/controllers/Base.php
@@ -27,7 +27,7 @@ class Base extends \Base_Controller {
             $data["content_view"] = "base/addUpdate";
             $data["mode"] = "add";
             $data["languages"]=explode("," ,$this->setting->translation_languages);
-            if($fileSizeValidation === false) alert("파일 업로드 용량은 2mb 이하만 가능합니다.");
+            if($fileSizeValidation === false) alert("업로드는 파일 하나당 2mb 이하만 가능합니다.");
             $this->template->render($data);
         }
         else{
