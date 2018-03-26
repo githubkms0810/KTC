fc2bfa3e9186781cd74d749f1cc1b5fc5f3f9ab6
kimmincastle <koreakms0810@gmail.com>
2018-03-26 17:29:31 +0900

    메타 정리

--- application/views/base/template.php
+++ application/views/base/template.php
@@ -3,7 +3,6 @@
     <head>
         <title>KTC | 코리아 통번역 센터</title>
         <meta charset="utf-8">
-        <meta name="description" content="">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="/public/images/favicon.ico">
