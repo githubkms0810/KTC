4e9d658f7f13246c2162e9c5c3af1d24684652e1
임수빈 <sbin910@gmail.com>
2018-04-10 18:36:35 +0900

    표만남음........................

--- application/modules/small/views/base/about_interpret.php
+++ application/modules/small/views/base/about_interpret.php
@@ -14,72 +14,15 @@
         </p>
         <a href="/translation_order/list" class="home-btn">포트폴리오 보러가기</a></div>
 </section>
-<section class="home-logo">
-    <div class="home-logo__wrap">
-        <ul class="home-logo--list">
-            <div>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-1.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-2.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-3.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-4.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-5.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-6.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-7.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-8.png">
-                </li>
-            </div>
-            <div>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-1.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-2.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-3.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-4.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-5.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-6.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-7.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-8.png">
-                </li>
-            </div>
-        </ul>
-    </div>
-</section>
+
 
 <!-- Start cat Area -->
-<section class="cat-area section-gap" id="service">
+<section class="cat-area section-gap" id="service" style="border-bottom:1px solid #eaeaea;">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-60 col-lg-8">
                 <div class="title text-center">
-                    <h1 class="mb-10">번역의뢰 하시기 전에 확인해 주세요.</h1>
+                    <h1 class="mb-10" style="font-size:40px;"><b>번역의뢰</b> 하시기 전에 확인해 주세요.</h1>
                     <p>번역의뢰 취소 하실 때에는 번역진행후 -시간 이내에 해주십시오.</p>
                 </div>
             </div>
@@ -87,7 +30,7 @@
         <div class="row">
             <div class="col-lg-4">
                 <div class="single-cat d-flex flex-column">
-                    <a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></a>
+                    <span class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></span>
                     <h4 class="mb-20" style="margin-top: 23px;">번역 가능 언어</h4>
                     <p>
                         한국어를 기본으로 영어/일본어/중국어/프랑스어/스페인어/독일어/러시아어/포르투갈어/태국어/베트남어/인도네시아어/기타 언어 번역가능합니다.
@@ -96,7 +39,7 @@
             </div>
             <div class="col-lg-4">
                 <div class="single-cat">
-                    <a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></a>
+                    <span class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></span>
                     <h4 class="mt-40 mb-20">파일 첨부시 유의사항</h4>
                     <p>
                         가급적 MS Word 프로그램으로 작성된 문서로 전달 부탁드리며, 부득이하게 PDF 파일로 보내주실시 견적 확인이 오래 걸릴 수 있습니다. 수정이 불가능한 PDF파일을 번역 하실 때에는 원본 문서와 동일하게 문서가 제작되지 않습니다.
@@ -105,21 +48,23 @@
             </div>
             <div class="col-lg-4">
                 <div class="single-cat">
-                    <a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-bug"></span></span></a>
+                    <span class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-bug"></span></span></span>
                     <h4 class="mt-40 mb-20">의뢰 취소 및 환불 규정</h4>
                     <p>
                         번역요금 견적 확인 후, 번역 진행을 원하실 때에는 <b>반드시</b> 견적서를 받으셨던 이메일로 [번역 진행 요청]회신을 보내주시길 바랍니다.
                     </p>
                 </div>
             </div>
+            
         </div>
+        
         <div class="row d-flex justify-content-center">
-            <div class="menu-content pb-60 col-lg-8">
+            <div class="menu-content col-lg-8">
                 <div class="title text-center">
                     <h1 class="mb-10">&nbsp;</h1>
                     <div style="width:213px; height:1px; background:#ced0e0; margin:0 auto;"></div>
                     <h3 class="mb-10">&nbsp;</h3>
-                    <p>* 취소 환불 규정<br> 번역진행 -시간 이내 100%<br> 번역진행 -시간 이후 번역 진행율에 따라 적용</p>
+                    <p>* 취소 환불 규정 *<br> 번역진행 -시간 이내 100%<br> 번역진행 -시간 이후 번역 진행율에 따라 적용</p>
                 </div>
             </div>
         </div>
@@ -129,26 +74,23 @@
 <!-- End cat Area -->
 
 <!-- Start price Area -->
-<section class="price-area section-gap" id="price">
+<section class="cat-area section-gap" id="price">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-60 col-lg-8">
                 <div class="title text-center">
-                    <h1 class="mb-10">Choose the Perfect Plan for you</h1>
-                    <p>Who are in extremely love with eco friendly system.</p>
+                    <h1 class="mb-10">번역 요금</h1>
+                    <p>MS Word / 한글 프로그램 기준<br>
+                    ** 원어민 감수는 번역 비용에 포함되어 있지않으며, 원어민 감수 진행시 번역료의 100%가 가산 됩니다. **</p>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="single-price">
                     <div class="top-sec d-flex justify-content-between">
                         <div class="top-left">
-                            <h4>Standard</h4>
-                            <p>For the <br>individuals</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£199</h1>
+                            <h4>한국어에서 외국어로 번역</h4>
                         </div>
                     </div>
                     <div class="bottom-sec">
@@ -168,15 +110,11 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="single-price">
                     <div class="top-sec d-flex justify-content-between">
                         <div class="top-left">
-                            <h4>Business</h4>
-                            <p>For the <br>small Company</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£399</h1>
+                            <h4>외국어에서 한국어로 번역</h4>
                         </div>
                     </div>
                     <div class="bottom-sec">
@@ -196,74 +134,22 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="single-price">
-                    <div class="top-sec d-flex justify-content-between">
-                        <div class="top-left">
-                            <h4>Ultimate</h4>
-                            <p>For the <br>large Company</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£499</h1>
-                        </div>
-                    </div>
-                    <div class="bottom-sec">
-                        <p>
-                            “Few would argue that, despite the advancements
-                        </p>
-                    </div>
-                    <div class="end-sec">
-                        <ul>
-                            <li>2.5 GB Free Photos</li>
-                            <li>Secure Online Transfer Indeed</li>
-                            <li>Unlimited Styles for interface</li>
-                            <li>Reliable Customer Service</li>
-                            <li>Manual Backup Provided</li>
-                        </ul>
-                        <button class="primary-btn price-btn mt-20">Purchase Plan</button>
-                    </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="menu-content col-lg-8">
+                <div class="title text-center">
+                    <h1 class="mb-10">&nbsp;</h1>
+                    <div style="width:213px; height:1px; background:#ced0e0; margin:0 auto;"></div>
+                    <h3 class="mb-10">&nbsp;</h3>
+                    <p>* 번역 완료 후 A/S *<br> 번역 결과물을 받으신 후, 오타/오역이 있을 시 수정요청 가능합니다.(3개월 내)<br>
+                        번역 결과물을 받으신 후, 내용 추가 하실 경우 20단어 미만 무상으로 번역서비스 해드립니다.(번역 완료 후 , 업무일 기준 3일 이내)  </p>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
 <!-- End price Area -->
-<!-- Start faq Area -->
-<section class="faq-area section-gap" id="faq">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="menu-content pb-60 col-lg-8">
-                <div class="title text-center">
-                    <h1 class="mb-10">환불 및 A/S</h1>
-                </div>
-            </div>
-        </div>
-        <div class="row d-flex align-items-center">
-            <div class="faq-content col-lg-9 col-md-9" style="margin:0 auto;">
-                <div class="single-faq">
-                    <h2>
-                        취소 환불 규정
-                    </h2>
-                    <p>
-                        번역 진행 -시간이내 100%<br>
-                        번역 진행 -시간 이후 번역 진행율에 따라 적용
-                    </p>
-                </div>
-                <div class="single-faq">
-                    <h2>
-                        번역 완료 후 A/S 관련
-                    </h2>
-                    <p>
-                        번역 결과물을 받으신 후, 오타/오역이 있을 시 수정요청 가능합니다.(3개월 내)<br>
-                        번역 결과물을 받으신 후, 내용 추가 하실 경우 20단어 미만 무상으로 번역서비스 해드립니다.(번역 완료 후 , 업무일 기준 3일 이내)                        
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- End faq Area -->
+
 <script>
     // Changing the defaults
     window.sr = ScrollReveal({
--- application/modules/small/views/base/about_translate.php
+++ application/modules/small/views/base/about_translate.php
@@ -14,99 +14,60 @@
         </p>
         <a href="/translation_order/list" class="home-btn">포트폴리오 보러가기</a></div>
 </section>
-<section class="home-logo">
-    <div class="home-logo__wrap">
-        <ul class="home-logo--list">
-            <div>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-1.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-2.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-3.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-4.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-5.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-6.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-7.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-8.png">
-                </li>
+<!-- Start about Area -->
+<section class="cat-area section-gap" style="border-bottom:1px solid #eaeaea;">
+    <div class="container">
+        <div class="row justify-content-center align-items-center d-flex">
+            <div class="col-lg-6 about-left">
+                <h1 style="font-size:40px; line-height:50px;">
+                    <b>통역의뢰</b>하시기 전에<br> 확인해주세요
+                </h1>
+                <p class="mt-20">
+                    한국어를 기본으로 영어/일본어/중국어/프랑스어/스페인어/독일어/<br>러시아어/포르투갈어/태국어/베트남어/인도네시아어/기타 언어 번역가능합니다. <br> 통역 서비스를 문의하실 때에 진행 사항에 따라 사용되는 통역종류가 다르니 참고하여 주십시오.
+                </p>
+                <a href="/translation_order/selectType" class="btn primary-btn text-uppercase mt-20">의뢰하기</a>
             </div>
-            <div>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-1.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-2.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-3.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-4.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-5.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-6.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-7.png">
-                </li>
-                <li class="home-logo--list__item">
-                    <img src="/public/images/logo-8.png">
-                </li>
+            <div class="col-lg-6 about-right">
+                <img class="img-fluid" src="/public/images/about-img.png" alt="">
             </div>
-        </ul>
+        </div>
     </div>
 </section>
-<!-- Start cat Area -->
-<section class="cat-area section-gap" id="service">
+<!-- End about Area -->
+<!-- Start service Area -->
+<section class="service-area section-gap" id="feature" style="border-bottom:1px solid #eaeaea;">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-60 col-lg-8">
                 <div class="title text-center">
-                    <h1 class="mb-10">통역의뢰 하시기 전에 확인해주세요</h1>
-                    <p>한국어를 기본으로 영어/일본어/중국어/프랑스어/스페인어/독일어/러시아어/포르투갈어/태국어/베트남어/인도네시아어/기타 언어 번역가능합니다. <br> 통역 서비스를 문의하실 때에 진행 사항에 따라 사용되는 통역종류가 다르니 참고하여 주십시오.</p>
+                    <h1 class="mb-10 home-title">통역 종류</h1>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4">
-                <div class="single-cat d-flex flex-column">
-                    <a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></a>
-                    <h4 class="mb-20" style="margin-top: 23px;">동시통역</h4>
-                    <p>
+            <div class="col-lg-4  col-md-6 mb-30">
+                <div class="single-service">
+                    <img class="d-block mx-auto mb-30" src="/public/images/f1.png" alt="">
+                    <h4 style="font-weight: bold;">동시통역</h4>
+                    <p class="ppp">
                         동시통역은 국제회의, 세미나, 심포지움, 워크샵 등에 적합합니다. 발표자의 발표내용을 실시간으로 통역하게 되며, 사전 발표자료 제공은 필수입니다. 각 언어당 통역사 2인이 한팀으로 통역하게 되며 동시통역을 위한 <b>동시통역 부스 및 시스템</b>, 청중을 위한 <b>리시버렌탈이 필요</b>합니다.(렌탈비용별도)
                     </p>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="single-cat">
-                    <a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></a>
-                    <h4 class="mt-40 mb-20">순차통역</h4>
-                    <p>
+            <div class="col-lg-4  col-md-6 mb-30">
+                <div class="single-service">
+                    <img class="d-block mx-auto mb-30" src="/public/images/f2.png" alt="">
+                    <h4 style="font-weight: bold;">순차통역</h4>
+                    <p class="ppp">
                         순차통역은 참석자가 제한된 소규모회의 등에 적합한 통역 방식입니다. 통역사 1인이 통역을 제공하게 되며, 발표자의 발표내용을 듣고 기록한 후, 청중들에게 내용을 다시 전달하는 방식입니다.
                     </p>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="single-cat">
-                    <a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-bug"></span></span></a>
-                    <h4 class="mt-40 mb-20">수행통역</h4>
-                    <p>
+            <div class="col-lg-4  col-md-6 mb-30">
+                <div class="single-service">
+                    <img class="d-block mx-auto mb-30" src="/public/images/f3.png" alt="">
+                    <h4 style="font-weight: bold;">수행통역</h4>
+                    <p class="ppp">
                         수행통역은 해외인사등 통역이 필요한 사람의 일정에 통역사가 수행원과 함께 다니며 통역을 제공하는 통역 방식입니다. 바이어 미팅등, 경우에 따라 순차통역으로 제공되어야 할 경우도 있습니다.
                     </p>
                 </div>
@@ -114,30 +75,25 @@
         </div>
     </div>
 </section>
-
-<!-- End cat Area -->
+<!-- End service Area -->
 
 <!-- Start price Area -->
-<section class="price-area section-gap" id="price">
+<section class="cat-area section-gap" id="price" style="border-bottom:1px solid #eaeaea;">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-60 col-lg-8">
                 <div class="title text-center">
-                    <h1 class="mb-10">통역요율</h1>
+                    <h1 class="mb-10 home-title">통역요율</h1>
                     <p>Who are in extremely love with eco friendly system.</p>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="single-price">
                     <div class="top-sec d-flex justify-content-between">
                         <div class="top-left">
-                            <h4>동시 통역</h4>
-                            <p>For the <br>individuals</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£199</h1>
+                            <h4>동시&순차 통역</h4>
                         </div>
                     </div>
                     <div class="bottom-sec">
@@ -145,9 +101,8 @@
                             <h4>영어/일본어/중국어</h4>
                         </div>
                         <div class="top-right">
-                            <h3>1시간까지    - 600,000원</h3>
-                            6시간까지      - 900,000원
-                            6시간 초과시   - 100,000원/시간
+                            <h3>1시간까지 - 600,000원</h3>
+                            6시간까지 - 900,000원 6시간 초과시 - 100,000원/시간
                         </div>
                     </div>
                     <div class="end-sec">
@@ -162,47 +117,11 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="single-price">
                     <div class="top-sec d-flex justify-content-between">
                         <div class="top-left">
-                            <h4>순차 통역</h4>
-                            <p>For the <br>small Company</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£399</h1>
-                        </div>
-                    </div>
-                    <div class="bottom-sec">
-                        <div class="top-left">
-                            <h4>순차 통역</h4>
-                            <p>For the <br>small Company</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£399</h1>
-                        </div>
-                    </div>
-                    <div class="end-sec">
-                        <ul>
-                            <li>2.5 GB Free Photos</li>
-                            <li>Secure Online Transfer Indeed</li>
-                            <li>Unlimited Styles for interface</li>
-                            <li>Reliable Customer Service</li>
-                            <li>Manual Backup Provided</li>
-                        </ul>
-                        <button class="primary-btn price-btn mt-20">Purchase Plan</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="single-price">
-                    <div class="top-sec d-flex justify-content-between">
-                        <div class="top-left">
-                            <h4>수행통역</h4>
-                            <p>For the <br>large Company</p>
-                        </div>
-                        <div class="top-right">
-                            <h1>£499</h1>
+                            <h4>수행 통역</h4>
                         </div>
                     </div>
                     <div class="bottom-sec">
@@ -227,13 +146,42 @@
     </div>
 </section>
 <!-- End price Area -->
-<!-- Start faq Area -->
-<section class="faq-area section-gap" id="faq">
+<!-- Start service Area -->
+<section class="service-area section-gap" id="feature">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-60 col-lg-8">
                 <div class="title text-center">
-                    <h1 class="mb-10">기타 비용</h1>
+                    <h1 class="mb-10 home-title">기타 비용</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4  col-md-6 mb-30">
+                <div class="single-service">
+                    <img class="d-block mx-auto mb-30" src="/public/images/f4.png" alt="">
+                    <h4 style="font-weight: bold;">출장료</h4>
+                    <p class="ppp">
+                        통역사들의 수도권 외 지역 출장시 1인 100,000원의 출장비용이 발생합니다. 왕복 교통비(항공, KTX, 고속버스, 택시, 유류비 등)와 숙박비용(호텔 기준) 발생 시, 실비 정산 또는 선지급 하여야 합니다.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4  col-md-6 mb-30">
+                <div class="single-service">
+                    <img class="d-block mx-auto mb-30" src="/public/images/f5.png" alt="">
+                    <h4 style="font-weight: bold;">사전 미팅 비용</h4>
+                    <p class="ppp">
+                        통역 업무전, 고객측의 필요에 의해 프리미팅을 진행 할 경우 1인 1시간 100,000원의 비용이 발생합니다.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4  col-md-6 mb-30">
+                <div class="single-service">
+                    <img class="d-block mx-auto mb-30" src="/public/images/f6.png" alt="">
+                    <h4 style="font-weight: bold;">녹음</h4>
+                    <p class="ppp">
+                         통역내용 녹음 시, 통역사와의 <b>사전 합의는 필수</b>입니다.(무단녹음 금지) 통역사와 합의 후, 녹음 시 통역료의 50%가 추가됩니다.(방송 및 온라인 업로드 100%)
+                    </p>
                 </div>
             </div>
         </div>
@@ -241,44 +189,17 @@
             <div class="faq-content col-lg-9 col-md-9" style="margin:0 auto;">
                 <div class="single-faq">
                     <h2>
-                        출장료
-                    </h2>
-                    <p>
-                        통역사들의 수도권 외 지역 출장시 1인 100,000원의 출장비용이 발생합니다.<br>
-                        왕복 교통비(항공, KTX, 고속버스, 택시, 유류비 등)와 숙박비용(호텔 기준) 발생 시, 실비 정산 또는 선지급 하여야 합니다.
-                    </p>
-                </div>
-                <div class="single-faq">
-                    <h2>
-                        사전 미팅 비용
-                    </h2>
-                    <p>
-                        통역 업무전, 고객측의 필요에 의해 프리미팅을 진행 할 경우 1인 1시간 100,000원의 비용이 발생합니다.                       
-                    </p>
-                </div>
-                <div class="single-faq">
-                    <h2>
                         취소 보상비용
                     </h2>
                     <p>
-                        표                   
-                    </p>
-                </div>
-                <div class="single-faq">
-                    <h2>
-                        녹음
-                    </h2>
-                    <p>
-                        통역내용 녹음 시, 통역사와의 <b>사전 합의는 필수</b>입니다.(무단녹음 금지)<br>
-                        통역사와 합의 후, 녹음 시 통역료의 50%가 추가됩니다.(방송 및 온라인 업로드 100%)
+                        표
                     </p>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- End faq Area -->
-
+<!-- End service Area -->
 <script>
     // Changing the defaults
     window.sr = ScrollReveal({
